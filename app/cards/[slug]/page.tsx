4179471e4b58709453736260bfe17beba7ca0503
Daniel Souza <dsouza1995@gmail.com>
2025-01-06 10:31:23 -0300

    feat: add mobile card page UI and card rules

--- app/cards/[slug]/page.tsx
+++ app/cards/[slug]/page.tsx
@@ -4,46 +4,114 @@ import { Globe } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
+interface Card {
+  id: string;
+  abilities: {
+    name: string;
+    text: string;
+  }[];
+  images: {
+    large: string;
+  };
+  name: string;
+  number: number;
+  rules: string[];
+  set: {
+    name: string;
+    series: string;
+    printedTotal: number;
+    ptcgoCode: string;
+  };
+  supertype: "Pokémon" | "Trainer" | "Energy";
+  tcgplayer: {
+    url: string;
+  };
+}
+
 export default async function Home({
   params,
 }: {
   readonly params: Promise<{ slug: string }>;
 }) {
+  const getCardRulesToDisplay = (card: Card) => {
+    if (card.supertype === "Trainer") {
+      // Remove the last rule for trainers, which is always a redundant, generic rule
+      return card.rules.slice(0, -1);
+    }
+    return card.rules;
+  };
+
+  const formatSetNumber = (number: number) => {
+    return number.toString().padStart(3, "0");
+  };
+
   const { slug } = await params;
-  const { data: pokemon } = await fetch(
-    `https://api.pokemontcg.io/v2/cards/${slug}`
+  const { data: card }: { data: Card } = await fetch(
+    `https://api.pokemontcg.io/v2/cards/${slug}`,
   ).then((response) => response.json());
 
   return (
-    <div className="flex flex-row gap-5 h-full items-center justify-center p-5 w-full">
-      <div className="flex flex-1 justify-center">
-        <Image
-          alt={pokemon.name}
-          src={pokemon.images.large}
-          width={320}
-          height={320}
-        />
-      </div>
-
-      <div className="flex flex-col flex-1 gap-5">
-        <p className="text-3xl font-bold">
-          {pokemon.name}
+    <div className="flex h-full w-full flex-col items-center gap-8 p-5 sm:flex-row sm:justify-center">
+      {/* Card title and set (mobile) */}
+      <div className="flex flex-col gap-1 self-start sm:hidden">
+        <p className="text-4xl font-bold">
+          {card.name}
           <span className="ml-3 text-xl font-normal text-muted-foreground">
-            ({pokemon.number}/{pokemon.set.printedTotal})
+            ({card.number}/{card.set.printedTotal})
           </span>
         </p>
 
-        <p className="text-base text-muted-foreground">
-          {pokemon.set.name} - {pokemon.set.series}
+        <p className="text-lg text-muted-foreground">
+          {card.set.name} - {card.set.series}
         </p>
+      </div>
 
-        <p className="text-sm text-muted-foreground pe-12">
-          &quot;{pokemon.rules[0]}&quot;
-        </p>
+      {/* Card image */}
+      <div className="flex flex-1 justify-center">
+        <Image
+          alt={card.name}
+          src={card.images.large}
+          width={224}
+          height={224}
+          className="my-5 self-center sm:hidden"
+        />
+        <Image
+          alt={card.name}
+          src={card.images.large}
+          width={320}
+          height={320}
+          className="my-5 hidden self-center sm:block"
+        />
+      </div>
 
-        <div className="flex flex-row gap-2">
+      <div className="flex flex-1 flex-col gap-5">
+        {/* Card title and set (desktop) */}
+        <div className="hidden flex-col gap-1 self-start sm:flex">
+          <p className="text-4xl font-bold">
+            {card.name}
+            <span className="ml-3 text-xl font-normal text-muted-foreground">
+              ({card.number}/{card.set.printedTotal})
+            </span>
+          </p>
+
+          <p className="text-lg text-muted-foreground">
+            {card.set.name} - {card.set.series}
+          </p>
+        </div>
+
+        {/* Card rules */}
+        {getCardRulesToDisplay(card).map((rule) => (
+          <blockquote className="text-center sm:text-left" key={rule}>
+            <p className="max-w-xl text-xs text-muted-foreground">
+              &quot;{rule}&quot;
+            </p>
+          </blockquote>
+        ))}
+
+        {/* Card seller links */}
+        <div className="flex flex-row justify-center gap-2 sm:justify-start">
           <Button asChild>
-            <Link href={pokemon.tcgplayer.url} target="_blank">
+            <Link href={card.tcgplayer.url} target="_blank">
               TCGPlayer
               <Globe />
             </Link>
@@ -52,10 +120,10 @@ export default async function Home({
           <Button asChild>
             <Link
               href={`https://www.ligapokemon.com.br/?view=cards/card&card=${encodeURIComponent(
-                pokemon.name
-              )}(${pokemon.number}/${pokemon.set.printedTotal})&ed=${
-                pokemon.set.ptcgoCode
-              }&num=${pokemon.number}`}
+                card.name,
+              )} (${formatSetNumber(card.number)}/${formatSetNumber(
+                card.set.printedTotal,
+              )})&ed=${card.set.ptcgoCode}&num=${formatSetNumber(card.number)}`}
               target="_blank"
             >
               Liga Pokémon
--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <Card className="bg-card shadow-indigo-500/40 py-3 px-4 border-0 flex items-center justify-between gap-6 rounded-2xl m-5">
+    <Card className="m-5 flex items-center justify-between gap-6 rounded-2xl border-0 bg-card px-4 py-3 shadow-indigo-500/40">
       <div className="flex items-center gap-2">
         <Image src="/pokeball.svg" alt="pokemon" height={24} width={24} />
         <p className="text-lg font-bold">Poké Library</p>
